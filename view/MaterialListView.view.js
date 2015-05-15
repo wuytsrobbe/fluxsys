fe2c87d1040fb281eb02dd1cd6a84ccf52786604
Robbe Wuyts <robbe_@live.be>
2015-05-15 08:29:32 +0000

    content veranderd van materiallistview page

--- view/MaterialListView.view.js
+++ view/MaterialListView.view.js
@@ -14,78 +14,80 @@ sap.ui.jsview("be.ordina.sap.view.MaterialListView", {
 	 */
 	createContent: function(oController) {
 		return new sap.m.Page({
-			title: "{i18n>appTitle}"
-		}).addContent(new sap.ui.comp.filterbar.FilterBar("filterBar", {
-			filterGroupItems: [
-			    new sap.ui.comp.filterbar.FilterGroupItem("fgiMaterialDesc", {
-		            groupTitle: "{i18n>material}",
-		            groupName: "material",
-					name: "fgiMaterial",
-					label: "{i18n>materialDesc}",
-					control: new sap.m.Input("inpMaterial", {
-						name: "inpMaterial"
-					}),
-					visibleInFilterBar: false
-					
-				}),
-				new sap.ui.comp.filterbar.FilterGroupItem("fgiMaterialNumber", {
-				     groupTitle: "{i18n>material}",
-		            groupName: "material",
-					name: "fgiMaterialNumber",
-					label: "{i18n>materialNumber}",
-					control: new sap.m.Input("inpMaterialNumber", {
-						name: "inpMaterialNumber"
-					}),
-					visibleInFilterBar: false
-				}),
-		            new sap.ui.comp.filterbar.FilterGroupItem("fgiMaterialType", {
-					groupTitle: "{i18n>type}",
-					groupName: "type",
-					name: "fgiMaterialType",
-					label: "{i18n>materialType}",
-					control: new sap.m.Input("inpMaterialType"),
-					visibleInAdvancedArea: true,
-					visibleInFilterBar: false
-				}),
-				new sap.ui.comp.filterbar.FilterGroupItem("fgiMaterialHierarchy", {
-					groupTitle: "{i18n>type}",
-					groupName: "type",
-					name: "fgiMaterialHierarchy",
-					label: "{i18n>hierarchy}",
-					control: new sap.m.Input("inpMaterialHierarchy"),
-					visibleInAdvancedArea: true,
-					visibleInFilterBar: false
-				}),
-					new sap.ui.comp.filterbar.FilterGroupItem("fgiDetailSteelQuality", {
-					groupTitle: "{i18n>detail}",
-					groupName: "detail",
-					name: "fgiDetailSteelQuality",
-					label: "{i18n>steelQuality}",
-					control: new sap.m.Input("inpDetailSteelQuality"),
-					visibleInAdvancedArea: true,
-					visibleInFilterBar: false
-				}),
-					new sap.ui.comp.filterbar.FilterGroupItem("fgiDetailDiameter", {
-					groupTitle: "{i18n>detail}",
-					groupName: "detail",
-					name: "fgiDetailDiameter",
-					label: "{i18n>diameter}",
-					control: new sap.m.Input("inpDetailDiameter"),
-					visibleInAdvancedArea: true,
-					visibleInFilterBar: false
-				}),
-					new sap.ui.comp.filterbar.FilterGroupItem("fgiDetailLength", {
-					groupTitle: "{i18n>detail}",
-					groupName: "detail",
-					name: "fgiDetailLength",
-					label: "{i18n>length}",
-					control: new sap.m.Input("inpDetailLength"),
-					visibleInAdvancedArea: true,
-					visibleInFilterBar: false
-				})
-		  ]
-		}));
+			title: "{i18n>appTitle}",
+			content: [
+			    new sap.ui.comp.filterbar.FilterBar("filterBar", {
+					filterGroupItems: [
+			        new sap.ui.comp.filterbar.FilterGroupItem("fgiMaterialDesc", {
+							groupTitle: "{i18n>material}",
+							groupName: "material",
+							name: "fgiMaterial",
+							label: "{i18n>materialDesc}",
+							control: new sap.m.Input("inpMaterial", {
+								name: "inpMaterial"
+							}),
+							visibleInFilterBar: false
 
+						}),
+				    new sap.ui.comp.filterbar.FilterGroupItem("fgiMaterialNumber", {
+							groupTitle: "{i18n>material}",
+							groupName: "material",
+							name: "fgiMaterialNumber",
+							label: "{i18n>materialNumber}",
+							control: new sap.m.Input("inpMaterialNumber", {
+								name: "inpMaterialNumber"
+							}),
+							visibleInFilterBar: false
+						}),
+		            new sap.ui.comp.filterbar.FilterGroupItem("fgiMaterialType", {
+							groupTitle: "{i18n>type}",
+							groupName: "type",
+							name: "fgiMaterialType",
+							label: "{i18n>materialType}",
+							control: new sap.m.Input("inpMaterialType"),
+							visibleInAdvancedArea: true,
+							visibleInFilterBar: false
+						}),
+				    new sap.ui.comp.filterbar.FilterGroupItem("fgiMaterialHierarchy", {
+							groupTitle: "{i18n>type}",
+							groupName: "type",
+							name: "fgiMaterialHierarchy",
+							label: "{i18n>hierarchy}",
+							control: new sap.m.Input("inpMaterialHierarchy"),
+							visibleInAdvancedArea: true,
+							visibleInFilterBar: false
+						}),
+					new sap.ui.comp.filterbar.FilterGroupItem("fgiDetailSteelQuality", {
+							groupTitle: "{i18n>detail}",
+							groupName: "detail",
+							name: "fgiDetailSteelQuality",
+							label: "{i18n>steelQuality}",
+							control: new sap.m.Input("inpDetailSteelQuality"),
+							visibleInAdvancedArea: true,
+							visibleInFilterBar: false
+						}),
+					new sap.ui.comp.filterbar.FilterGroupItem("fgiDetailDiameter", {
+							groupTitle: "{i18n>detail}",
+							groupName: "detail",
+							name: "fgiDetailDiameter",
+							label: "{i18n>diameter}",
+							control: new sap.m.Input("inpDetailDiameter"),
+							visibleInAdvancedArea: true,
+							visibleInFilterBar: false
+						}),
+					new sap.ui.comp.filterbar.FilterGroupItem("fgiDetailLength", {
+							groupTitle: "{i18n>detail}",
+							groupName: "detail",
+							name: "fgiDetailLength",
+							label: "{i18n>length}",
+							control: new sap.m.Input("inpDetailLength"),
+							visibleInAdvancedArea: true,
+							visibleInFilterBar: false
+						})
+		  ]
+				})
+			    ]
+		});
 	}
 
 });
