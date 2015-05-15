3f6a69d23cb993c6a80f01a1d5f545b49634ab5a
Gert Vermeersch <gert.vermeersch@ordina.be>
2015-05-15 08:17:31 +0000

    Copy version to git

---
+++ Component.js
@@ -0,0 +1,95 @@
+jQuery.sap.declare("be.ordina.sap.Component");
+jQuery.sap.require("sap.ui.core.routing.History");
+jQuery.sap.require("sap.m.routing.RouteMatchedHandler");
+jQuery.sap.require("sap.ui.core.UIComponent");
+
+
+sap.ui.core.UIComponent.extend("be.ordina.sap.Component", {
+	metadata: {
+		name: "Material Search",
+		version: "0.1",
+		dependencies: {
+			libs: ["sap.m", "sap.ui.layout", "sap.ui.comp"]
+		},
+		
+		rootView: {
+			viewName: "be.ordina.sap.view.App",
+			type: "JS"
+		},
+		config: {
+			resourceBundle: "i18n/messageBundle.properties",
+			serviceConfig: {
+				name: "",
+				serviceUrl: ""
+			}
+		},
+		routing: {
+			config: {
+			//	routerClass: be.ordina.sap.MyRouter,
+				viewType: "JS", //sap.ui.core.mvc.ViewType.JS,
+				viewPath: "be.ordina.sap.view",
+				targetAggregation: "pages",
+				targetControl: "app",
+				clearTarget: false
+			},
+			routes: [
+				{
+					pattern: "",
+					name: "materialListview",
+					view: "MaterialListView"
+
+			}]
+		}
+	},
+
+	init: function() {
+	
+		sap.ui.core.UIComponent.prototype.init.apply(this, arguments);
+		var deviceModel = new sap.ui.model.json.JSONModel({
+			isTouch: sap.ui.Device.support.touch,
+			isNoTouch: !sap.ui.Device.support.touch,
+			isPhone: sap.ui.Device.system.phone,
+			isNoPhone: !sap.ui.Device.system.phone,
+			listMode: sap.ui.Device.system.phone ? "None" : "SingleSelectMaster",
+			listItemType: sap.ui.Device.system.phone ? "Active" : "Inactive"
+		});
+		deviceModel.setDefaultBindingMode("OneWay");
+		
+		var sRootPath = jQuery.sap.getModulePath("be.ordina.sap");
+		var oServiceConfig = this.getMetadata().getConfig().serviceConfig;
+        var sServiceUrl = oServiceConfig.serviceUrl;
+        
+        var mConfig = this.getMetadata().getConfig();
+		
+		// the metadata is read to get the location of the i18n language files later
+       
+        //this._routeMatchedHandler = new sap.m.routing.RouteMatchedHandler(this.getRouter(), this._bRouterCloseDialogs);
+
+        // create oData model
+        //TODO nog te schrijven
+        //this._initODataModel(sServiceUrl);
+
+        // set i18n model
+        var i18nModel = new sap.ui.model.resource.ResourceModel({
+            bundleUrl : [ sRootPath, mConfig.resourceBundle ].join("/")
+        });
+        this.setModel(i18nModel, "i18n");
+
+		this.setModel(deviceModel, "device");
+		this.getRouter().initialize();
+
+	},
+
+	createContent: function() {
+		var oViewData = {
+			component: this
+		};
+
+		return sap.ui.view({
+			viewName: "be.ordina.sap.view.App",
+			type: sap.ui.core.mvc.ViewType.JS,
+			viewData: oViewData
+		});
+	}
+
+});
---
+++ view/MaterialListView.view.js
@@ -0,0 +1,91 @@
+sap.ui.jsview("be.ordina.sap.view.MaterialListView", {
+
+	/** Specifies the Controller belonging to this View. 
+	 * In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	 * @memberOf be.ordina.sap.view.MaterialListView
+	 */
+	getControllerName: function() {
+		return "be.ordina.sap.controller.MaterialListView";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	 * Since the Controller is given to this method, its event handlers can be attached right away.
+	 * @memberOf be.ordina.sap.view.MaterialListView
+	 */
+	createContent: function(oController) {
+		return new sap.m.Page({
+			title: "{i18n>appTitle}"
+		}).addContent(new sap.ui.comp.filterbar.FilterBar("filterBar", {
+			filterGroupItems: [
+			    new sap.ui.comp.filterbar.FilterGroupItem("fgiMaterialDesc", {
+		            groupTitle: "{i18n>material}",
+		            groupName: "material",
+					name: "fgiMaterial",
+					label: "{i18n>materialDesc}",
+					control: new sap.m.Input("inpMaterial", {
+						name: "inpMaterial"
+					}),
+					visibleInFilterBar: false
+					
+				}),
+				new sap.ui.comp.filterbar.FilterGroupItem("fgiMaterialNumber", {
+				     groupTitle: "{i18n>material}",
+		            groupName: "material",
+					name: "fgiMaterialNumber",
+					label: "{i18n>materialNumber}",
+					control: new sap.m.Input("inpMaterialNumber", {
+						name: "inpMaterialNumber"
+					}),
+					visibleInFilterBar: false
+				}),
+		            new sap.ui.comp.filterbar.FilterGroupItem("fgiMaterialType", {
+					groupTitle: "{i18n>type}",
+					groupName: "type",
+					name: "fgiMaterialType",
+					label: "{i18n>materialType}",
+					control: new sap.m.Input("inpMaterialType"),
+					visibleInAdvancedArea: true,
+					visibleInFilterBar: false
+				}),
+				new sap.ui.comp.filterbar.FilterGroupItem("fgiMaterialHierarchy", {
+					groupTitle: "{i18n>type}",
+					groupName: "type",
+					name: "fgiMaterialHierarchy",
+					label: "{i18n>hierarchy}",
+					control: new sap.m.Input("inpMaterialHierarchy"),
+					visibleInAdvancedArea: true,
+					visibleInFilterBar: false
+				}),
+					new sap.ui.comp.filterbar.FilterGroupItem("fgiDetailSteelQuality", {
+					groupTitle: "{i18n>detail}",
+					groupName: "detail",
+					name: "fgiDetailSteelQuality",
+					label: "{i18n>steelQuality}",
+					control: new sap.m.Input("inpDetailSteelQuality"),
+					visibleInAdvancedArea: true,
+					visibleInFilterBar: false
+				}),
+					new sap.ui.comp.filterbar.FilterGroupItem("fgiDetailDiameter", {
+					groupTitle: "{i18n>detail}",
+					groupName: "detail",
+					name: "fgiDetailDiameter",
+					label: "{i18n>diameter}",
+					control: new sap.m.Input("inpDetailDiameter"),
+					visibleInAdvancedArea: true,
+					visibleInFilterBar: false
+				}),
+					new sap.ui.comp.filterbar.FilterGroupItem("fgiDetailLength", {
+					groupTitle: "{i18n>detail}",
+					groupName: "detail",
+					name: "fgiDetailLength",
+					label: "{i18n>length}",
+					control: new sap.m.Input("inpDetailLength"),
+					visibleInAdvancedArea: true,
+					visibleInFilterBar: false
+				})
+		  ]
+		}));
+
+	}
+
+});
